ccda9e7c3d2cac5bf5541377eef12ce29a0c0895
MathieuBuisson <matthieu.buisson@gmail.com>
2018-05-06 18:02:03 +0100

    Try packaging the extension

---
+++ PSCodeHealth/task/EntryPoint.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Position=0, Mandatory=$False)]
+    [ValidateScript({ Test-Path $_ })]
+    [string]$SourceCodePath,
+    
+    [Parameter(Position=1, Mandatory=$False)]
+    [ValidateScript({ Test-Path $_ })]
+    [string]$TestsPath,
+
+    [Parameter(Mandatory=$False)]
+    [switch]$Recurse
+)
+
+Write-Verbose "Loading PSCodeHealth module shipped in the VSTS extension"
+Import-Module "$PSScriptRoot\ps_modules\0.2.9\PSCodeHealth.psd1" -Force
+
+Write-Verbose "Loading PSScriptAnalyzer module shipped in the VSTS extension"
+Import-Module "$PSScriptRoot\ps_modules\PSScriptAnalyzer\PSScriptAnalyzer.psd1" -Force
+
+Write-Host "Value of SourceCodePath : $SourceCodePath"
+Write-Host "Value of TestsPath : $TestsPath"
+Write-Host "Value of Recurse : $($Recurse)"
+Write-Host "Type of Recurse : $($Recurse.GetType().FullName)"
