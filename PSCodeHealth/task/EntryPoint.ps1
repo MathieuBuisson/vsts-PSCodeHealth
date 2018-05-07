d28c243fa00f59d660261748261f0cf61990a5d1
MathieuBuisson <matthieu.buisson@gmail.com>
2018-05-07 17:48:44 +0100

    Add more logging even if System.Debug is not True

--- PSCodeHealth/task/EntryPoint.ps1
+++ PSCodeHealth/task/EntryPoint.ps1
@@ -6,16 +6,12 @@ Trace-VstsEnteringInvocation $MyInvocation
 Try {
     Import-Module "$PSScriptRoot\Helpers.psm1" -Force -Verbose
     Get-TaskInputs
-    Write-TaskInputs -Verbose
+    Write-TaskInputs
     Import-Dependencies
     
     $PSVersionTable.PSVersion
     
     Get-Module -Name 'Pester' -ListAvailable
-    Get-Module -Name 'PackageManagement' -ListAvailable
-
-    '------------------------------- Variables --------------------------------'
-    Get-VstsTaskVariableInfo
 }
 Finally {
     Trace-VstsLeavingInvocation $MyInvocation
--- PSCodeHealth/task/Helpers.psm1
+++ PSCodeHealth/task/Helpers.psm1
@@ -3,7 +3,6 @@ Function Get-TaskInputs {
     Param()
 
     [string]$Global:RootDirectoryPath = Get-VstsTaskVariable -Name 'Build.SourcesDirectory'
-    Write-VstsTaskVerbose -Message "Value of RootDirectoryPath : $RootDirectoryPath"
 
     [string]$Global:SourceCodePath = Get-VstsInput -Name SourceCodePath -Default $RootDirectoryPath
     [string]$Global:TestsPath = Get-VstsInput -Name TestsPath -Default $RootDirectoryPath
@@ -14,21 +13,21 @@ Function Write-TaskInputs {
     [CmdletBinding()]
     Param()
 
-    Write-VstsTaskVerbose -Message "Value of RootDirectoryPath : $RootDirectoryPath"
-    
-    Write-VstsTaskVerbose -Message "Value of SourceCodePath : $SourceCodePath"
-    Write-VstsTaskVerbose -Message "Value of TestsPath : $TestsPath"
-    Write-VstsTaskVerbose -Message "Value of Recurse : $($Recurse)"
+    "Value of RootDirectoryPath : $RootDirectoryPath"
+
+    "Value of SourceCodePath : $SourceCodePath"
+    "Value of TestsPath : $TestsPath"
+    "Value of Recurse : $($Recurse)"
 }
 
 Function Import-Dependencies {
     [CmdletBinding()]
     Param()
 
-    Write-VstsTaskVerbose -Message "Loading PSScriptAnalyzer module shipped in the VSTS extension"
+    'Loading PSScriptAnalyzer module shipped in the VSTS extension'
     Import-Module "$PSScriptRoot\ps_modules\PSScriptAnalyzer\PSScriptAnalyzer.psd1" -Force
 
-    Write-VstsTaskVerbose -Message "Loading PSCodeHealth module shipped in the VSTS extension"
+    'Loading PSCodeHealth module shipped in the VSTS extension'
     Import-Module "$PSScriptRoot\ps_modules\PSCodeHealth\PSCodeHealth.psd1" -Force
 }
 
