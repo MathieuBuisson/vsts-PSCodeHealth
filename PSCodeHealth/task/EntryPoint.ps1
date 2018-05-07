902dea9a9205f4ee5c5fef8e9fd4189abfa504b6
MathieuBuisson <matthieu.buisson@gmail.com>
2018-05-07 11:50:45 +0100

    Update PowerShell task handler

--- PSCodeHealth/task/EntryPoint.ps1
+++ PSCodeHealth/task/EntryPoint.ps1
@@ -9,7 +9,7 @@ Param(
     [string]$TestsPath,
 
     [Parameter(Mandatory=$False)]
-    [switch]$Recurse
+    [bool]$Recurse = $False
 )
 
 Write-Verbose "Loading PSCodeHealth module shipped in the VSTS extension"
@@ -17,6 +17,10 @@ Import-Module "$PSScriptRoot\ps_modules\0.2.9\PSCodeHealth.psd1" -Force
 
 Write-Verbose "Loading PSScriptAnalyzer module shipped in the VSTS extension"
 Import-Module "$PSScriptRoot\ps_modules\PSScriptAnalyzer\PSScriptAnalyzer.psd1" -Force
+
+$PSVersionTable.PSVersion
+
+Get-Module -Name 'Pester' -ListAvailable
 
 Write-Host "Value of SourceCodePath : $SourceCodePath"
 Write-Host "Value of TestsPath : $TestsPath"
