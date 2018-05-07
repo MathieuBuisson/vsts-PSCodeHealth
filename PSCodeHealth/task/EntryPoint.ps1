93acadb37925c0ee197e2ce4217293892ac87c4a
MathieuBuisson <matthieu.buisson@gmail.com>
2018-05-07 13:58:53 +0100

    Use the VstsTaskSdk module and include it in the package

--- PSCodeHealth/task/EntryPoint.ps1
+++ PSCodeHealth/task/EntryPoint.ps1
@@ -1,28 +1,22 @@
 [CmdletBinding()]
-Param(
-    [Parameter(Position=0, Mandatory=$False)]
-    [ValidateScript({ Test-Path $_ })]
-    [string]$SourceCodePath,
+Param()
+
+Trace-VstsEnteringInvocation $MyInvocation
+
+Try {
+    Import-Module "$PSScriptRoot\Helpers.psm1" -Force -Verbose
+    Get-TaskInputs
+    Write-TaskInputs -Verbose
+    Import-Dependencies
     
-    [Parameter(Position=1, Mandatory=$False)]
-    [ValidateScript({ Test-Path $_ })]
-    [string]$TestsPath,
+    $PSVersionTable.PSVersion
+    
+    Get-Module -Name 'Pester' -ListAvailable
+    Get-Module -Name 'PackageManagement' -ListAvailable
 
-    [Parameter(Mandatory=$False)]
-    [bool]$Recurse = $False
-)
-
-Write-Verbose "Loading PSCodeHealth module shipped in the VSTS extension"
-Import-Module "$PSScriptRoot\ps_modules\0.2.9\PSCodeHealth.psd1" -Force
-
-Write-Verbose "Loading PSScriptAnalyzer module shipped in the VSTS extension"
-Import-Module "$PSScriptRoot\ps_modules\PSScriptAnalyzer\PSScriptAnalyzer.psd1" -Force
-
-$PSVersionTable.PSVersion
-
-Get-Module -Name 'Pester' -ListAvailable
-
-Write-Host "Value of SourceCodePath : $SourceCodePath"
-Write-Host "Value of TestsPath : $TestsPath"
-Write-Host "Value of Recurse : $($Recurse)"
-Write-Host "Type of Recurse : $($Recurse.GetType().FullName)"
+    '------------------------------- Variables --------------------------------'
+    Get-VstsTaskVariableInfo
+}
+Finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
---
+++ PSCodeHealth/task/Helpers.psm1
@@ -0,0 +1,30 @@
+Function Get-TaskInputs {
+    [CmdletBinding()]
+    Param()
+
+    [string] $SourceCodePath = Get-VstsInput -Name SourceCodePath
+    [string] $TestsPath = Get-VstsInput -Name TestsPath
+    [bool] $Recurse = Get-VstsInput -Name Recurse -AsBool
+}
+
+Function Write-TaskInputs {
+    [CmdletBinding()]
+    Param()
+
+    Write-VstsTaskVerbose -Message "Value of SourceCodePath : $SourceCodePath"
+    Write-VstsTaskVerbose -Message "Value of TestsPath : $TestsPath"
+    Write-VstsTaskVerbose -Message "Value of Recurse : $($Recurse)"
+}
+
+Function Import-Dependencies {
+    [CmdletBinding()]
+    Param()
+
+    Write-VstsTaskVerbose -Message "Loading PSScriptAnalyzer module shipped in the VSTS extension"
+    Import-Module "$PSScriptRoot\ps_modules\PSScriptAnalyzer\PSScriptAnalyzer.psd1" -Force
+
+    Write-VstsTaskVerbose -Message "Loading PSCodeHealth module shipped in the VSTS extension"
+    Import-Module "$PSScriptRoot\ps_modules\PSCodeHealth\PSCodeHealth.psd1" -Force
+}
+
+Export-ModuleMember -Function '*'
