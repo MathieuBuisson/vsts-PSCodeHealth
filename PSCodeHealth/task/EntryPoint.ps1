224c56959eb8cc0ab8f2c6927cdfa3a72ba3d770
MathieuBuisson <matthieu.buisson@gmail.com>
2018-05-07 21:55:44 +0100

    Add functionality to generate an HTML report

--- PSCodeHealth/task/EntryPoint.ps1
+++ PSCodeHealth/task/EntryPoint.ps1
@@ -4,14 +4,19 @@ Param()
 Trace-VstsEnteringInvocation $MyInvocation
 
 Try {
-    Import-Module "$PSScriptRoot\Helpers.psm1" -Force -Verbose
-    Get-TaskInputs
-    Write-TaskInputs
+    'Loading helpers into the current scope'
+    . "$PSScriptRoot\Helpers.ps1"
+
+    $PSCodeHealthParams = Get-PSCodeHealthParamsFromInputs
+    Write-PSCodeHealthParamsFromInputs $PSCodeHealthParams
     Import-Dependencies
-    
-    $PSVersionTable.PSVersion
-    
-    Get-Module -Name 'Pester' -ListAvailable
+
+    'Running the PowerShell code analysis ...'
+    $HealthReport = Invoke-PSCodeHealth @PSCodeHealthParams
+    'Overall report view :'
+    $HealthReport
+    'Per-function report view :'
+    $HealthReport.FunctionHealthRecords | Format-Table
 }
 Finally {
     Trace-VstsLeavingInvocation $MyInvocation
---
+++ PSCodeHealth/task/Helpers.ps1
@@ -0,0 +1,57 @@
+Function Get-PSCodeHealthParamsFromInputs {
+    [CmdletBinding()]
+    Param()
+
+    [string]$RootDirectoryPath = Get-VstsTaskVariable -Name 'Build.SourcesDirectory'
+    [string]$StagingDirectory = Get-VstsTaskVariable -Name 'Build.ArtifactStagingDirectory'
+
+    $InputsHashTable = @{
+        Path = Get-VstsInput -Name SourceCodePath -Default $RootDirectoryPath
+        TestsPath = Get-VstsInput -Name TestsPath -Default $RootDirectoryPath
+        Recurse = Get-VstsInput -Name Recurse -AsBool
+    }
+
+    If ( Get-VstsInput -Name GenerateHtmlReport -AsBool ) {
+        $HtmlReportPath = Get-VstsInput -Name HtmlReportPath -Default "$StagingDirectory\PSCodeHealthReport.html"
+        $InputsHashTable.Add('HtmlReportPath', $HtmlReportPath)
+
+        # To output both an HTML file and a [PSCodeHealth.Overall.HealthReport] object
+        $InputsHashTable.Add('PassThru', $True)
+    }
+    return $InputsHashTable
+}
+
+Function Write-PSCodeHealthParamsFromInputs {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory, Position=0)]
+        [Hashtable]$InputsHashTable
+    )
+
+    Foreach ( $InputEntry in $InputsHashTable.GetEnumerator() ) {
+        "Value of input [$($InputEntry.Key)] : $($InputEntry.Value)"
+    }
+}
+
+Function Import-Dependencies {
+    [CmdletBinding()]
+    Param()
+
+    $IsPesterInstalled = Get-Module -Name 'Pester' -ListAvailable
+    If ( $IsPesterInstalled ) {
+        'The module [Pester] is already present, skipping installation.'
+    }
+    Else {
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        'Installing module [Pester] from the PowerShell Gallery ...'
+        Install-Module 'Pester' -Scope CurrentUser -Force -SkipPublisherCheck
+    }
+    'Loading module [Pester]'
+    Import-Module 'Pester' -Force
+
+    'Loading module [PSScriptAnalyzer] shipped in the VSTS extension'
+    Import-Module "$PSScriptRoot\ps_modules\PSScriptAnalyzer\PSScriptAnalyzer.psd1" -Force
+
+    'Loading module [PSCodeHealth] shipped in the VSTS extension'
+    Import-Module "$PSScriptRoot\ps_modules\PSCodeHealth\PSCodeHealth.psd1" -Force
+}
